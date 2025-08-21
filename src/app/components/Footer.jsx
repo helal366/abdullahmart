39351e836cd24d552e441450176c985e492c2e95
Helal366 <helal56876@gmail.com>
2025-08-21 16:28:28 +0600

    Logo and web page name added in footer.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <section className='padding py-16 bg-green-200'>
-        <p className='my-5'>abdullahMart</p>
-        <img src="/logo1" alt="Logo"  className='h-16'/>
+        <p className='my-5 font-semibold text-green-950'>abdullahMart</p>
+        <img src="/logo1.png" alt="Logo"  className='h-16 border-2 border-green-700 rounded-lg'/>
     </section>
   )
 }
